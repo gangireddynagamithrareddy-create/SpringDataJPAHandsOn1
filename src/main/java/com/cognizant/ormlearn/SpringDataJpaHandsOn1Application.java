ca1f795d7134c6eba8a3e3e846798ea37ef462cb
gangireddynagamithrareddy-create <gangireddynagamithrareddy-create@gmail.com>
2026-07-12 11:15:22 +0530

    Completed Spring Data JPA Hands-on 1

---
+++ src/main/java/com/cognizant/ormlearn/SpringDataJpaHandsOn1Application.java
@@ -0,0 +1,13 @@
+package com.cognizant.ormlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaHandsOn1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaHandsOn1Application.class, args);
+    }
+
+}
