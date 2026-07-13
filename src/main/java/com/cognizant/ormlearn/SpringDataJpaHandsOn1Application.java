a6959c8b4dbe8ff17c36c87d049e6f410389e4a5
gangireddynagamithrareddy-create <gangireddynagamithrareddy-create@gmail.com>
2026-07-13 21:52:12 +0530

    Completed Spring Data JPA HandsOn 1 - Exercise 1

--- src/main/java/com/cognizant/ormlearn/SpringDataJpaHandsOn1Application.java
+++ src/main/java/com/cognizant/ormlearn/SpringDataJpaHandsOn1Application.java
@@ -1,13 +1,41 @@
 package com.cognizant.ormlearn;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
 
 @SpringBootApplication
 public class SpringDataJpaHandsOn1Application {
 
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringDataJpaHandsOn1Application.class);
+
+    private static CountryService countryService;
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringDataJpaHandsOn1Application.class, args);
+
+        ApplicationContext context =
+                SpringApplication.run(SpringDataJpaHandsOn1Application.class, args);
+
+        countryService = context.getBean(CountryService.class);
+
+        testGetAllCountries();
     }
 
+    private static void testGetAllCountries() {
+        LOGGER.info("Start");
+
+        List<Country> countries = countryService.getAllCountries();
+
+        System.out.println(countries);
+
+        LOGGER.info("End");
+    }
 }
